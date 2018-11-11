2887ae78d823c170efb02b39ea4d8cd7e7319c7a
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2018-11-11 19:31:16 +0530

    added library and cli

--- index.js
+++ index.js
@@ -1,71 +1,47 @@
-const isUp = require('is-up');
-const got = require('got');
-const defaultOptions = {
-	showMeta: false
-};
+#!/usr/bin/env node
+'use strict';
+const meow = require('meow');
+const blameMyNetwork = require('./lib/index');
 
-const blameMyNetwork = async (site, opts = {}) => {
-	if (!site) {
-		return new Error('Please specify site URL');
-	}
-	opts = Object.assign({}, defaultOptions, opts);
-	const requestStack = {};
-	const _generateResponse = (status, reason) => {
-		if (opts.showMeta) {
-			return Object.assign({}, { meta: requestStack }, { status, reason });
-		} else {
-			return Object.assign({}, { status, reason });
-		}
-	};
+const helpText = `
+Usage
+  $ blamemynet <website>
 
-	try {
-		requestStack.externalConnection = false;
-		let foreignNetworkResult = await isUp(site);
-		requestStack.externalConnection = foreignNetworkResult;
-		requestStack.externalConnectionReason = foreignNetworkResult ? 'Success' : 'Failure';
-	} catch (error) {
-		if (error.code === 'ENOTFOUND') {
-			requestStack.externalConnectionReason = "Either site doesn't exists or you are not connected to internet";
-		} else if (error.message === 'Invalid domain') {
-			return _generateResponse(false, 'You have entered Invalid domain');
-		} else {
-		}
-		requestStack.externalConnectionReason = `${error.code ? error.code : error.name} : ${error.message}`;
-	}
-
-	try {
-		requestStack.internalConnection = false;
-		let internalNetworkResult = await got(site, { timeout: 5000 });
-		requestStack.internalConnection = true;
-		requestStack.internalConnectionTimings = internalNetworkResult.timings;
-		requestStack.internalConnectionReason = internalNetworkResult ? 'Success' : 'Failue';
-	} catch (error) {
-		if (error.code === 'ENOTFOUND') {
-			requestStack.internalConnectionReason = "Either site doesn't exists or you are not connected to internet";
-		} else {
-			requestStack.internalConnectionReason = `${error.code ? error.code : error.name} : ${error.message}`;
+Options
+  --informative, -i  Include additional information for network
+  --output-json, -j Log details in JSON format
+Examples
+  $ blamemynet http://theanubhav.com
+  🌈 unicorns 🌈
+`;
+const cli = meow(helpText, {
+	flags: {
+		informative: {
+			type: 'boolean',
+			alias: 'i'
+		},
+		'output-json': {
+			type: 'boolean',
+			alias: 'j'
 		}
 	}
+});
 
-	//should only let the user know that his network could not access the website
-	if (requestStack.externalConnection) {
-		if (requestStack.internalConnection) {
-			return _generateResponse(true, 'Connection successful from both, internal and external network.');
-		} else {
-			return _generateResponse(false, 'Blame you network. The external network can acess the site.');
-		}
-	} else {
-		if (requestStack.internalConnection) {
-			return _generateResponse(false, 'Only internal network could access the site.');
-		} else {
-			return _generateResponse(false, 'Connection failure from both, internal and external network');
-		}
-	}
-};
+if (cli.input.length === 0) {
+	console.error('Error: Please specify website to check');
+	console.log(helpText);
+	process.exit(1);
+}
 
 (async () => {
-	let result = await blameMyNetwork('http://theanubhav.com', { showMeta: true });
-	console.log(result);
+	let result = await blameMyNetwork(cli.input[0], { showMeta: cli.flags['i'] });
+	if (cli.flags['j']) {
+		console.log(result);
+	} else {
+		console.log(result.reason);
+		if (cli.flags['i']) {
+			console.log(`Additional Details : ${JSON.stringify(result.meta, null, 4)}`);
+		}
+	}
+	process.exit(result.status ? 0 : 2);
 })();
-
-module.exports = blameMyNetwork;
